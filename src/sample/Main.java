d2770bd2b5106a3e538c057447c08fbb864e1c0a
Ekaterina Gerasina <gerasinaes@mail.ru>
2020-05-18 12:11:51 +0400

    .

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,24 +1,49 @@
 package sample;
 
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.StrictMath.sqrt;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-       // stage.setTitle("Hello World");
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+
+        Application.launch(args);
     }
 
+    @Override
+    public void start(Stage stage) {
 
-    public static void main(String[] args) {
-        launch(args);
+
+        Text text = new Text("");
+        text.setLayoutY(200);
+        text.setLayoutX(200);
+
+        Group group = new Group(text);
+
+        Scene scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setTitle("Minesweeper");
+        stage.setWidth(200);
+        stage.setHeight(200);
+        stage.show();
     }
 }
